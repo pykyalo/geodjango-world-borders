0753c413216d4eeb7c338c35673308edb45f7354
Steven Kyalo <skyalo100@gmail.com>
2024-06-18 12:03:51 +0300

    feature: adding django admin and loaded data

--- world/admin.py
+++ world/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
+from django.contrib.gis import admin
+from .models import WorldBorder
 
-# Register your models here.
+
+class WorldBorderAdmin(admin.ModelAdmin):
+    list_display = ["name", "fips", "iso2", "iso3", "region", "subregion", "mpoly"]
+    search_fields = ("name", "fips", "iso2", "iso3")
+
+admin.site.register(WorldBorder, admin.GISModelAdmin)
--- world/models.py
+++ world/models.py
@@ -1,3 +1,35 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+class WorldBorder(models.Model):
+    """ ogrinfo -so world/data/TM_WORLD_BORDERS-0.3.shp TM_WORLD_BORDERS-0.3
+    
+    Gives:
+    FIPS: String (2.0)
+    ISO2: String (2.0)
+    ISO3: String (3.0)
+    UN: Integer (3.0)
+    NAME: String (50.0)
+    AREA: Integer (7.0)
+    POP2005: Integer64 (10.0)
+    REGION: Integer (3.0)
+    SUBREGION: Integer (3.0)
+    LON: Real (8.3)
+    LAT: Real (7.3)
+    """
+    
+    name = models.CharField(max_length=50)
+    area = models.IntegerField()
+    pop2005 = models.IntegerField("Population 2005")
+    fips = models.CharField("FIPS Code", max_length=2, null=True, blank=True)
+    iso2 = models.CharField(max_length=2)
+    iso3 = models.CharField(max_length=3)
+    un = models.IntegerField("United Nations Code")
+    region = models.IntegerField()
+    subregion = models.IntegerField()
+    lon = models.FloatField()
+    lat = models.FloatField()
+
+    mpoly = models.MultiPolygonField()
+
+    def __str__(self):
+        return self.name
